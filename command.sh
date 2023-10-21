81d0a8fb155d470b14f12ab3ec07540c26ba3a48
Andrii Lishchyna <andrej044@i.ua>
2023-10-21 18:22:55 -0400

    add link to repository

--- command.sh
+++ command.sh
@@ -1,3 +1,5 @@
+# REPO_URL <https://github.com/Andrej044/forage__blackbird>
+
 1.  npx create-react-app forage__blackbird;
 2.  gh repo create forage__blackbird --public --source=. --remote=upstream
 3.  git checkout -b update_logo
