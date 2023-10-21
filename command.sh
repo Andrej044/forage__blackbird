553c804bee2272558127b230196f81577fb786fc
Andrii Lishchyna <andrej044@i.ua>
2023-10-21 18:21:12 -0400

    create command list

---
+++ command.sh
@@ -0,0 +1,12 @@
+1.  npx create-react-app forage__blackbird;
+2.  gh repo create forage__blackbird --public --source=. --remote=upstream
+3.  git checkout -b update_logo
+4.  git add . 
+    git commit -m "replace logo.svg"
+    
+5.  git add . 
+    git commit -m "replace link on https://www.propelleraero.com/dirtmate/"
+    git push --set-upstream master update_logo
+6.  gh pr status
+    gh pr create
+7.  gh pr merge
